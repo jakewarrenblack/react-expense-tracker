5807e2fdfa7e0a716b63441d4957289dcef456ab
Jake <jakewarrenblack01@gmail.com>
2022-06-07 20:10:11 +0100

    Style AddTransaction form

--- src/components/AddTransaction.tsx
+++ src/components/AddTransaction.tsx
@@ -1,18 +1,41 @@
+import { useState } from "react";
+
 export const AddTransaction = () => {
+  const [text, setText] = useState("");
+  const [amount, setAmount] = useState(0);
+
   return (
     <div>
-      <h3>Add new transaction</h3>
-      <form>
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input type="text" placeholder="Enter text..." />
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount">
-            Amount <br />
-            (negative - expense, positive - income)
+      <h3 className="border-2 border-b-gray-400 pb-2 text-lg font-semibold">
+        Add new transaction
+      </h3>
+      <form className="space-y-4">
+        <div className="flex flex-col mt-4">
+          <label htmlFor="text" className="font-semibold">
+            Text
           </label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="text"
+            id="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Enter text..."
+          />
+        </div>
+        <div className="flex flex-col">
+          <label htmlFor="amount" className="font-semibold">
+            Amount{" "}
+            <span className="text-sm">
+              (negative - expense, positive - income)
+            </span>
+          </label>
+          <input
+            className="mt-2"
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(parseInt(e.target.value))}
+            placeholder="Enter amount..."
+          />
         </div>
       </form>
     </div>
