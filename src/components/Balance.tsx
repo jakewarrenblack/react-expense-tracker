def3562c7ac6d794d423fe155bd122b9774c5e50
Jake <jakewarrenblack01@gmail.com>
2022-06-08 00:31:55 +0100

    Calculating income/expenses

--- src/components/Balance.tsx
+++ src/components/Balance.tsx
@@ -1,8 +1,20 @@
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
+
 export const Balance = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  // just getting an array of amounts
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  // don't really get what this is doing, must look further into reduce()
+  // it's adding them all together anyway
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
   return (
     <>
       <h4 className="text-lg">YOUR BALANCE</h4>
-      <h1 className="font-semibold text-2xl">$0.00</h1>
+      <h1 className="font-semibold text-2xl">${total}</h1>
     </>
   );
 };
--- src/components/IncomeExpenses.tsx
+++ src/components/IncomeExpenses.tsx
@@ -1,13 +1,36 @@
+import { GlobalContext } from "../context/GlobalState";
+import { useContext } from "react";
+
 export const IncomeExpenses = () => {
+  // to me this seems like using something like redux?
+  // we can keep whatever we want in the state and access it wherever it's needed
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+
+  // these methods are confusing to me, specifically the reduce()
+
+  // calculate total income
+  const income = amounts
+    .filter((item) => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  // calculate total expense
+  const expense = amounts
+    .filter((item) => item < 0)
+    .reduce((acc, item) => (acc += item), 0 * -1)
+    .toFixed(2);
+
   return (
     <div className="flex w-full justify-between">
       <div className="">
         <h4>Income</h4>
-        <p className="text-green-700">+$0.00</p>
+        <p className="text-green-700">{income}</p>
       </div>
       <div className="0">
         <h4>Expense</h4>
-        <p className="text-red-700">-$0.00</p>
+        <p className="text-red-700">{expense}</p>
       </div>
     </div>
   );
